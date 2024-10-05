a95cbd4863ee45e1aac09c055bf6d109734b4625
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-05 09:37:50 +0100

    fix the probleme of sticking the sidebar by adding the overflow proprty to the box of content

--- src/router/layouts/DashboardLayout.tsx
+++ src/router/layouts/DashboardLayout.tsx
@@ -5,86 +5,9 @@ import Sidebar from "../../pages/private/components/Sidebar";
 const DashboardLayout = () => {
   return (
     <Fragment>
-      <div className="d-flex gap-2 position-relative" style={{height: "100%"}}>
+      <div className="d-flex gap-2 bg-danger" style={{height: "100%"}}>
         <Sidebar/>
-        <div className="p-2 w-100">
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
-          outlet of the sidebar<br/>
+        <div className="p-2 w-100 h-auto overflow-y-auto">
           outlet of the sidebar<br/>
           <Outlet />
         </div>
