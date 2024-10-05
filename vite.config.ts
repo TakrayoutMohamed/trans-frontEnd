900944515eaa0e87eb441685fc7be1fa03e7752e
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-05 13:16:26 +0100

    setup the aliases for files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve:{
+    alias:{
+      "@" : path.resolve(__dirname, "."),
+      "@src" : path.resolve(__dirname, "./src"),
+      "@assets" : path.resolve(__dirname, "./public/assets"),
+      "@icons" : path.resolve(__dirname, "./public/assets/icons"),
+      "@images" : path.resolve(__dirname, "./public/assets/images"),
+      "@videos" : path.resolve(__dirname, "./public/assets/videos"),
+      "@router": path.resolve(__dirname, "./src/router"),
+      "@publicPages": path.resolve(__dirname, "./src/pages/public"),
+      "@privatePages": path.resolve(__dirname, "./src/pages/private"),
+      "@publicComponents": path.resolve(__dirname, "./src/pages/public/components"),
+      "@privateComponents": path.resolve(__dirname, "./src/pages/private/components")
+    }
+  }
 })
