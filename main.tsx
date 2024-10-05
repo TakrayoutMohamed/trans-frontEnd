fad33708336f673197ee1088cad19ba26015edb6
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-05 09:39:19 +0100

    importing the js scripts by installing popperjs/core

--- main.tsx
+++ main.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./src/App.tsx";
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 import "bootstrap/dist/js/bootstrap.js";
+import "bootstrap/dist/js/bootstrap.esm.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 import "./main.css";
 
 const rootDOM = document.getElementById("root") as Element;
