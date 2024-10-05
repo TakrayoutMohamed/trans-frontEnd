46dab4444e856f8d387b2634d3c7a6d939e8a6e1
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-05 18:40:32 +0100

    update the path of icons to make it as a root path based on the public

--- media-exporting.ts
+++ media-exporting.ts
@@ -1,6 +1,6 @@
-export const brandIcon = './assets/icons//brandIcon.svg';
-export const homeIcon = './assets/icons/homeIcon.svg';
-export const profileIcon = './assets/icons/profileIcon.svg';
-export const gameIcon = './assets/icons/gameIcon.svg';
-export const chatIcon = './assets/icons/chatIcon.svg';
-export const settingsIcon = './assets/icons/settingsIcon.svg';
+export const brandIcon = '/assets/icons//brandIcon.svg';
+export const homeIcon = '/assets/icons/homeIcon.svg';
+export const profileIcon = '/assets/icons/profileIcon.svg';
+export const gameIcon = '/assets/icons/gameIcon.svg';
+export const chatIcon = '/assets/icons/chatIcon.svg';
+export const settingsIcon = '/assets/icons/settingsIcon.svg';
