aa51ed19d51d5ae21979226901911f2a2cce1985
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-08 17:51:36 +0100

    adding the private routing protection and authorization protection

--- src/router/MainRoutingComponent.tsx
+++ src/router/MainRoutingComponent.tsx
@@ -1,9 +1,9 @@
 import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouteObject,
-	RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouteObject,
+  RouterProvider,
 } from "react-router-dom";
 
 // layouts
@@ -30,39 +30,47 @@ import FriendProfile from "@privatePages/FriendProfile";
 import Details from "@privatePages/Details";
 import Password from "@privatePages/Password";
 
+// routers protection
+import PrivateRoutes from "@router/PrivateRoutes";
+import AuthorizationRoutes from "@router/AuthorizationRoutes";
+
 const routingTree: RouteObject[] = createRoutesFromElements(
-	<Route element={<RootLayout />}>
-		<Route path="/" element={<HomeLayout />}> {/*landing page routing */}
-			<Route index element={<Home />} /> {/* home page*/}
-			<Route path="about-us" element={<AboutUs />} /> {/* about us*/}
-			<Route path="team" element={<Team />} /> {/* team*/}
-			<Route path="contact" element={<Contact />} /> {/* contact*/}
-		</Route>
-		<Route path="sign-up" element={<SignUp />} /> {/* sign up*/}
-		<Route path="sign-in" element={<SignIn />} /> {/* sign in*/}
-		<Route  element={<DashboardLayout />}>
-			<Route path="game" element={<Game />} /> {/*game */}
-			<Route path="profile" element={<ProfileLayout />}> {/*profile layout */}
-				<Route index element={<Profile />} /> {/* profile */}
-				<Route path='recent' element={<Recent />} /> {/* Recent */}
-				<Route path='friends' element={<FriendsLayout />} > {/* Friends layout*/}
-					<Route index element={<Friends />}/> {/* Friends */}
-					<Route path=":userName" element={<FriendProfile />}/> {/* Friend Profile */}
-				</Route>
-			</Route >
-			<Route path="chat" element={<Chat />} /> {/*chat */}
-			<Route path="setting" element={<SettingLayout/>}> {/*setting layout */}
-				<Route index element={<Details />} /> {/*details */}
-				<Route path="password" element={<Password />} /> {/*password */}
-			</Route>
-		</Route>
-	</Route>
+  <Route element={<RootLayout />}>
+    <Route path="/" element={<HomeLayout />}>{/*landing page routing */}
+      <Route index element={<Home />} /> {/* home page*/}
+      <Route path="about-us" element={<AboutUs />} /> {/* about us*/}
+      <Route path="team" element={<Team />} /> {/* team*/}
+      <Route path="contact" element={<Contact />} /> {/* contact*/}
+    </Route>
+    <Route element={<AuthorizationRoutes />}>
+      <Route path="sign-up" element={<SignUp />} /> {/* sign up*/}
+      <Route path="sign-in" element={<SignIn />} /> {/* sign in*/}
+    </Route>
+    <Route element={<PrivateRoutes />}>
+      <Route element={<DashboardLayout />}>
+        <Route path="game" element={<Game />} /> {/*game */}
+        <Route path="profile" element={<ProfileLayout />}>{/*profile layout */}
+          <Route index element={<Profile />} /> {/* profile */}
+          <Route path="recent" element={<Recent />} /> {/* Recent */}
+          <Route path="friends" element={<FriendsLayout />}>{/* Friends layout*/}
+            <Route index element={<Friends />} /> {/* Friends */}
+            <Route path=":userName" element={<FriendProfile />} />{/* Friend Profile */}
+          </Route>
+        </Route>
+        <Route path="chat" element={<Chat />} /> {/*chat */}
+        <Route path="setting" element={<SettingLayout />}>{/*setting layout */}
+          <Route index element={<Details />} /> {/*details */}
+          <Route path="password" element={<Password />} /> {/*password */}
+        </Route>
+      </Route>
+    </Route>
+  </Route>
 );
 
 const router = createBrowserRouter(routingTree);
 
 const MainRoutingComponent = () => {
-	return <RouterProvider router={router}></RouterProvider>;
+  return <RouterProvider router={router}></RouterProvider>;
 };
 
 export default MainRoutingComponent;
