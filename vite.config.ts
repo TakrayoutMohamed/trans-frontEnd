f096fd790aa4533a7b7096ac5c31f9689cc5650d
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-08 12:03:59 +0100

    adding the path of states and change the places arrangment of the files declaration

--- vite.config.ts
+++ vite.config.ts
@@ -8,15 +8,16 @@ export default defineConfig({
   resolve:{
     alias:{
       "@" : path.resolve(__dirname, "."),
-      "@src" : path.resolve(__dirname, "./src"),
       "@assets" : path.resolve(__dirname, "/assets"),
       "@icons" : path.resolve(__dirname, "/assets/icons"),
       "@images" : path.resolve(__dirname, "/assets/images"),
       "@videos" : path.resolve(__dirname, "/assets/videos"),
+      "@src" : path.resolve(__dirname, "./src"),
       "@router": path.resolve(__dirname, "./src/router"),
+      "@states": path.resolve(__dirname, "./src/states"),
       "@publicPages": path.resolve(__dirname, "./src/pages/public"),
-      "@privatePages": path.resolve(__dirname, "./src/pages/private"),
       "@publicComponents": path.resolve(__dirname, "./src/pages/public/components"),
+      "@privatePages": path.resolve(__dirname, "./src/pages/private"),
       "@privateComponents": path.resolve(__dirname, "./src/pages/private/components")
     }
   }
