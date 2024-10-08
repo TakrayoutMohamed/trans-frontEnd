074c1540b744318429e454c33bb67ac900a71daa
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-08 17:50:27 +0100

    adding the store to the project

--- main.tsx
+++ main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "@src/App.tsx";
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.bundle.js";
+// import "bootstrap/dist/js/bootstrap.bundle.js";//it breaks the drop down proprites
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap/dist/js/bootstrap.esm.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "@/main.css";
+import { Provider } from "react-redux";
+import { store } from "@states/store";
 
 const rootDOM = document.getElementById("root") as Element;
 
@@ -15,6 +17,8 @@ const root = createRoot(rootDOM);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
