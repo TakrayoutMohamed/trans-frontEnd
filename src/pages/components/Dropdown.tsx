bc3397f96d2551b798e5c2c64d4946203b5765bc
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-13 14:38:25 +0100

    drop down styling
    update the drop down by custumizing its style instead of the default one , also adding an horizontal line befor the last item of the drop down items

--- src/pages/components/Dropdown.tsx
+++ src/pages/components/Dropdown.tsx
@@ -10,15 +10,18 @@ type DropdownProps = {
 
 const Dropdown = ({className, linksDetails, children }: DropdownProps) => {
   return (
-    <div className={`${className} dropdown`}>
-      <span className="" data-bs-toggle="dropdown" aria-expanded="false">
+    <div className={`${className} dropdown bg-danger`}>
+      <span className="" data-bs-toggle="dropdown" aria-expanded="true">
         {children}
       </span>
-      <ul className="dropdown-menu mx-auto">
-        {linksDetails.map((content, index) => (
-          <li key={index} className="dropdown-item">
-            {content.data}
-          </li>
+      <ul className="dropdown-menu bg-success-subtle border-0 p-0 m-1">
+        {linksDetails && linksDetails.map((content, index) => (
+          <>
+            {linksDetails.length - 1 === index && <hr className="mx-2 my-0 mb-1"/>}
+            <li key={index} className="dropdown-item py-2">
+              {content.data}
+            </li>
+          </>
         ))}
       </ul>
     </div>
