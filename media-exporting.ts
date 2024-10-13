ac5698db339c83c17ad8ca69995ce08861beed8d
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-13 21:50:12 +0100

    adding the export of ping pong image

--- media-exporting.ts
+++ media-exporting.ts
@@ -1,6 +1,10 @@
+//icons
 export const brandIcon = '/assets/icons//brandIcon.svg';
 export const homeIcon = '/assets/icons/homeIcon.svg';
 export const profileIcon = '/assets/icons/profileIcon.svg';
 export const gameIcon = '/assets/icons/gameIcon.svg';
 export const chatIcon = '/assets/icons/chatIcon.svg';
 export const settingsIcon = '/assets/icons/settingsIcon.svg';
+
+//images
+export const pingPongVector = '/assets/images/pingPongVector.svg';
