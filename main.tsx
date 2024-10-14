9beae49ae7710368b9a1126dac493ec5a26bd53d
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-14 18:56:44 +0100

    change the arrangment of import to use the bootstrap in all components

--- main.tsx
+++ main.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "@src/App.tsx";
 import "bootstrap/dist/css/bootstrap.css";
-// import "bootstrap/dist/js/bootstrap.bundle.js";//it breaks the drop down proprites
+// import "bootstrap/dist/js/bootstrap.bundle.min.js";//it breaks the drop down proprites
 import "bootstrap/dist/js/bootstrap.js";
 import "bootstrap/dist/js/bootstrap.esm.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "@/main.css";
+import { createRoot } from "react-dom/client";
+import App from "@src/App.tsx";
 import { Provider } from "react-redux";
 import { store } from "@states/store";
+import { StrictMode } from "react";
 
 const rootDOM = document.getElementById("root") as Element;
 
 const root = createRoot(rootDOM);
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </StrictMode>
 );
