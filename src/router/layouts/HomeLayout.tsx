bcb79c8e2722a71650b55ab4c95382872a9b0b75
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-14 19:46:53 +0100

    wrap  home layout in a div

--- src/router/layouts/HomeLayout.tsx
+++ src/router/layouts/HomeLayout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "@publicComponents/Navbar";
+import styles from "@router/styles/HomeLayout.module.css";
 
 const HomeLayout = () => {
   return (
     <>
-      <div className="bg-info">HomeLayout</div>
-      <header>
-        <Navbar></Navbar>
-      </header>
-      <Outlet />
+      <div className={"bg-danger " + styles['homeLayout']}>
+        <header>
+          <Navbar></Navbar>
+        </header>
+        <Outlet />
+      </div>
     </>
   );
 };
