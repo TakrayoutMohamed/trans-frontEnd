aa97889c082e74ca8e420c6eb2fc6f9c390aa28c
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-14 20:54:38 +0100

    update the styling of the home.tsx to make it look as in the design

--- src/pages/public/Home.tsx
+++ src/pages/public/Home.tsx
@@ -1,49 +1,45 @@
 import { Fragment } from "react/jsx-runtime";
-// import styles from "./styles/Home.module.css";
-import HeaderDescription, {
-  Props,
+import styles from "@publicPages/styles/Home.module.css";
+import {
+  Description,
+  Heading,
 } from "@/src/pages/public/components/HeaderAndDescription";
 import { pingPongVector } from "@/media-exporting";
 
 const Home = () => {
-  const firstComponent: Props = {
-    header: { className: "h1 ", content: "this is the header" },
-    description: {
-      className: "",
-      content:
-        "Lorem ipsum delit. Repudiarrem ipsum delit. Repudiarrem ipsum delit. Repudiar.",
-    },
-  };
   return (
     <Fragment>
-      <main>
-        <section className="position-relative row bg-danger">
-          <div className="col-md-12 col-xs-6 my-auto position-absolute z-1">
-            <HeaderDescription
-              header={firstComponent.header}
-              description={firstComponent.description}
-            />
+      <main className={"position-relative p-2 pt-0 "}>
+        <section className="bg-dangerr position-relative mb-5 mt-3" style={{ height: "24em" }} >
+          <div className="bg-successs position-absolute z-1 d-flex justify-content-end flex-column h-100  ps-4" style={{width:"65%"}}>
+            <div className="h1 ">BEST <span style={{color:"#EE0DFF"}}>GAME</span></div>
+            <div className="h1 ">PLAYING TODAY</div>
+            <br/>
+            <div className="mb-2">
+              We captivate a multitude of users with our game. Our unwavering mission is to spread the thrill  and excitement of ping pong to every corner of the world.
+            </div>
           </div>
-          <div className="col-md-12 col-xs-6 position-absolute z-0">
+          <div
+            className="bg-success-subtlee position-absolute z-0 top-0 end-0 d-flex flex-row-reverse"
+            style={{ height: "100%", width: "60%" }}
+          >
             <img
               src={pingPongVector}
               alt="pingPongVector"
-              width="400px"
-              className="float-end bg-success  img-fluid z-0"
+              width="500px"
+              className="bg-infso img-fluid "
+              style={{ minWidth: "", maxWidth: "80%", marginRight:"15%" }}
             />
           </div>
         </section>
-        <section>
-          {/* <HeaderDescription
-            header={firstComponent.header}
-            description={firstComponent.description}
-          /> */}
+        <section className="bg-danger" style={{marginTop:"150px"}}>
+          <Heading className="">this is the header of section 2 </Heading>
+          <Description className="">
+            Lorem ipsum delit. Repudiarrem ipsum delit. Repudiarrem ipsum delit.
+            Repudiar RepudiarRepudiarRepudiarRepudiar.
+          </Description>
         </section>
         <section>
-          {/* <HeaderDescription
-            header={firstComponent.header}
-            description={firstComponent.description}
-          /> */}
         </section>
       </main>
       <footer></footer>
