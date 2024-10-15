a818380c16508b25e5b9f33c432eba1ab309543b
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-15 14:15:12 +0100

    Exporting the images of landing page

--- media-exporting.ts
+++ media-exporting.ts
@@ -5,6 +5,11 @@ export const profileIcon = '/assets/icons/profileIcon.svg';
 export const gameIcon = '/assets/icons/gameIcon.svg';
 export const chatIcon = '/assets/icons/chatIcon.svg';
 export const settingsIcon = '/assets/icons/settingsIcon.svg';
+export const playVideoIcon = '/assets/icons/playVideoIcon.svg';
 
 //images
 export const pingPongVector = '/assets/images/pingPongVector.svg';
+export const snowPingPongTable = '/assets/images/snowPingPongTable.svg';
+export const forestPingPongTable = '/assets/images/forestPingPongTable.svg';
+export const desertPingPongTable = '/assets/images/desertPingPongTable.svg';
+export const videoCaption = '/assets/images/videoCaption.svg';
