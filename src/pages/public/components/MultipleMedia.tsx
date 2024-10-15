206ab6dea4ca615bf4debca04a1910d9734049fa
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-15 14:16:38 +0100

    update multiMedia component to work with eather string or ReactNodes

--- src/pages/public/components/MultipleMedia.tsx
+++ src/pages/public/components/MultipleMedia.tsx
@@ -1,11 +1,11 @@
-interface Media {
-  data:[{}]
+interface MediaProps {
+  children?:  string[] | React.ReactNode;
+  className: string;
 }
 
-const MultipleMedia = () => {
-  return (
-    <div>MultipleMedia</div>
-  )
-}
+const MultipleMedia = ({ children, className="" }: MediaProps) => {
+  if (!children) return <div>No media to show</div>;
+  return <div className={className}>{children}</div>;
+};
 
-export default MultipleMedia
+export default MultipleMedia;
