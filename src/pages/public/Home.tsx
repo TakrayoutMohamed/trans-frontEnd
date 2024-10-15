a453d0e8a8ba0d630ef8c0bd11302115bffed9b9
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-15 14:19:34 +0100

    Home pages update styling
    in HomeLayout style update the radiant of background to work with as in design
    
    Home.tsx : continue implimenting the design

--- src/pages/public/Home.tsx
+++ src/pages/public/Home.tsx
@@ -1,22 +1,36 @@
 import { Fragment } from "react/jsx-runtime";
-import styles from "@publicPages/styles/Home.module.css";
+import { home } from "@publicPagesStyles/index";
+import { Description, Heading } from "@publicComponents/HeaderAndDescription";
 import {
-  Description,
-  Heading,
-} from "@/src/pages/public/components/HeaderAndDescription";
-import { pingPongVector } from "@/media-exporting";
+  pingPongVector,
+  snowPingPongTable,
+  forestPingPongTable,
+  desertPingPongTable,
+  videoCaption,
+} from "@/media-exporting";
+import MultipleMedia from "./components/MultipleMedia";
 
 const Home = () => {
   return (
     <Fragment>
-      <main className={"position-relative p-2 pt-0 "}>
-        <section className="bg-dangerr position-relative mb-5 mt-3" style={{ height: "24em" }} >
-          <div className="bg-successs position-absolute z-1 d-flex justify-content-end flex-column h-100  ps-4" style={{width:"65%"}}>
-            <div className="h1 ">BEST <span style={{color:"#EE0DFF"}}>GAME</span></div>
+      <main className={`position-relative p-2 pt-0 ${home}`}>
+        <section
+          className="bg-dangerr position-relative mt-3"
+          style={{ height: "24rem" }}
+        >
+          <div
+            className="bg-successs position-absolute z-1 d-flex justify-content-end flex-column h-100  ps-4"
+            style={{ width: "65%" }}
+          >
+            <div className="h1 ">
+              BEST <span style={{ color: "#EE0DFF" }}>GAME</span>
+            </div>
             <div className="h1 ">PLAYING TODAY</div>
-            <br/>
+            <br />
             <div className="mb-2">
-              We captivate a multitude of users with our game. Our unwavering mission is to spread the thrill  and excitement of ping pong to every corner of the world.
+              We captivate a multitude of users with our game. Our unwavering
+              mission is to spread the thrill and excitement of ping pong to
+              every corner of the world.
             </div>
           </div>
           <div
@@ -28,18 +42,47 @@ const Home = () => {
               alt="pingPongVector"
               width="500px"
               className="bg-infso img-fluid "
-              style={{ minWidth: "", maxWidth: "80%", marginRight:"15%" }}
+              style={{ minWidth: "", maxWidth: "80%", marginRight: "15%" }}
             />
           </div>
         </section>
-        <section className="bg-danger" style={{marginTop:"150px"}}>
-          <Heading className="">this is the header of section 2 </Heading>
-          <Description className="">
-            Lorem ipsum delit. Repudiarrem ipsum delit. Repudiarrem ipsum delit.
-            Repudiar RepudiarRepudiarRepudiarRepudiar.
+        <section
+          className="bg-dangerr d-flex flex-column justify-content-center p-4 "
+          style={{ marginTop: "10rem",background: "#080228",
+        }}
+        >
+          <Heading className="h1 text-center pb-3">
+            BEST <span style={{ color: "#EE0DFF" }}>GAMING</span> EXPERIENCE
+          </Heading>
+          <Description className="text-center ">
+            by give you multiple maps and different universe
           </Description>
+          <MultipleMedia className="row py-5">
+            <img src={snowPingPongTable} alt="snow ping pong table" className="col-4 " />
+            <img src={forestPingPongTable} alt="forest ping pong table" className="col-4" />
+            <img src={desertPingPongTable} alt="desert ping pong table" className="col-4" />
+          </MultipleMedia>
         </section>
-        <section>
+        <section
+          className="bg-dangerr d-flex flex-column justify-content-center p-4 "
+          style={{ marginTop: "10rem",background: "",
+        }}
+        >
+          <Heading className="h1 text-center ">
+            OUR GOAL IS TO CREATE <span style={{ color: "#EE0DFF" }}>LIFELIKE</span>
+          </Heading>
+          <Heading className="h1 text-center pb-3">
+            <span style={{ color: "#EE0DFF" }}>VIRTUAL</span> TABLE TENNIS EXPERIENCES
+          </Heading>
+          <Description className="text-center ">
+            Our mission to entertain the world goes beyond gaming. Our teams create innovative and
+          </Description>
+          <Description className="text-center ">
+            thrilling ping pong experiences for everyone. Here, I will explain the user experience (UX) of our website.
+          </Description>
+          <MultipleMedia className="row py-5">
+            <img src={videoCaption} alt="video caption" className="" />
+          </MultipleMedia>
         </section>
       </main>
       <footer></footer>
@@ -48,3 +91,4 @@ const Home = () => {
 };
 
 export default Home;
+
