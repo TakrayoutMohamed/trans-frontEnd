0a0047f2200a694d31dcf8deaf20d5faac79b87c
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-16 10:37:16 +0100

    LandingPage responsive
    make the landing page responsive to look more good in diffirent screens

--- src/router/layouts/HomeLayout.tsx
+++ src/router/layouts/HomeLayout.tsx
@@ -5,11 +5,15 @@ import styles from "@router/styles/HomeLayout.module.css";
 const HomeLayout = () => {
   return (
     <>
-      <div className={"bg-danger " + styles['homeLayout']}>
-        <header>
-          <Navbar></Navbar>
-        </header>
-        <Outlet />
+      <div className={"bg-dangerr d-flex  " + styles['homeLayout']}>
+        <div className="col-md-0 col-lg-2 col-xl-2"></div>
+        <div className={"shadow-lg col-md-12 col-lg-8 col-xl-8 " + styles['homeLayout']}>
+          <header>
+            <Navbar></Navbar>
+          </header>
+          <Outlet />
+        </div>
+        <div className="col-md-0 col-lg-2 col-xl-2"></div>
       </div>
     </>
   );
