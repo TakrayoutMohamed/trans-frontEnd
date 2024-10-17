a768449374fcfe6fd89a95934af2a4fbf3f1781a
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-17 14:06:57 +0100

    Finishing the style of registerLayout

--- src/router/layouts/RegistrationLayout.tsx
+++ src/router/layouts/RegistrationLayout.tsx
@@ -1,21 +1,27 @@
 import { Outlet } from "react-router-dom";
 import { Fragment } from "react/jsx-runtime";
-import { registrationLayout } from "../styles";
+import { registrationLayout ,registrationLayoutStick, registrationLayoutSignUpIn } from "@router/styles";
 import { ballIcon } from "@/media-exporting";
+import { BiSolidRightArrow } from "react-icons/bi";
 
 const RegistrationLayout = () => {
   return (
     <Fragment>
-      <div className={"bg-info-subtle container "+ registrationLayout}>
-        ads
-        <div className="bg-success-subtle col row m-0">
-          <div className="bg-success col-8 row mx-0">
-            <div className=" bg-primary-subtle" style={{width:"30em"}}>cont stick</div>
-            <div className="border border-2">sign-(in-up)</div>
-            <Outlet />
+      <div className={"bg-info d-flex "+ registrationLayout}>
+        <div className="bg-success-subtle container row m-auto" style={{height:"40em"}}>
+          <div className="bg-success col-9 d-flex mx-0" >
+            <div className="bg-danger-subtle w-100">
+              <Outlet />
+            </div>
+            <div className={`border border-2 d-flex my-auto mx-3 p-1  ${registrationLayoutSignUpIn}`} >
+              <p className="bg-dangerr text-center h4 m-auto">
+                SIGN <BiSolidRightArrow className="m-0 me-1" size="1em"/> IN
+              </p>
+            </div>
+            <div className={` bg-primary-subtlee my-auto ${registrationLayoutStick} `} ></div>
           </div>
-          <div className="bg-danger-subtle col-4 d-flex">
-            <img src={ballIcon} alt="" width="40em" className="mx-auto" />
+          <div className="bg-danger-subtle col-2 d-flex ms-auto" >
+            <img src={ballIcon} alt="the ball icon" width="80em" className="ms-auto" />
           </div>
         </div>
       </div>
