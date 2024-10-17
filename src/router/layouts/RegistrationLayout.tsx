70d9ad924d24d02a6e331f9183026730ce48ced0
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-17 20:28:26 +0100

    adding a start animation function to handle the switch from a sing-in to sign-up

--- src/router/layouts/RegistrationLayout.tsx
+++ src/router/layouts/RegistrationLayout.tsx
@@ -1,27 +1,50 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { Fragment } from "react/jsx-runtime";
 import { registrationLayout ,registrationLayoutStick, registrationLayoutSignUpIn } from "@router/styles";
 import { ballIcon } from "@/media-exporting";
-import { BiSolidRightArrow } from "react-icons/bi";
+import { BiSolidRightArrow, BiSolidLeftArrow } from "react-icons/bi";
 
 const RegistrationLayout = () => {
+  const {pathname} = useLocation();
+  const navigate = useNavigate();
+  const sign = (pathname === "/sign-in" ? "UP" : "IN");
+  const startAnimation = () : void => {
+    const switching = document.querySelector(".signInUp");
+    if (pathname === "/sign-in")
+    {
+      switching?.classList?.remove("flex-row-reverse");
+      navigate("/sign-up");
+      
+    }
+    if (pathname === "/sign-up")
+    {
+      switching?.classList.add("flex-row-reverse");
+      navigate("/sign-in");
+    }
+  }
   return (
     <Fragment>
       <div className={"bg-info d-flex "+ registrationLayout}>
-        <div className="bg-success-subtle container row m-auto" style={{height:"40em"}}>
-          <div className="bg-success col-9 d-flex mx-0" >
-            <div className="bg-danger-subtle w-100">
+        <div className="bg-success-subtle container row m-auto p-0" style={{height:"40em"}}>
+          <div className="bg-success col-12 col-sm-11 col-md-10 col-lg-9 d-flex  signInUp mx-0" >
+            <div className="bg-danger-subtle w-100 ">
               <Outlet />
             </div>
-            <div className={`border border-2 d-flex my-auto mx-3 p-1  ${registrationLayoutSignUpIn}`} >
-              <p className="bg-dangerr text-center h4 m-auto">
-                SIGN <BiSolidRightArrow className="m-0 me-1" size="1em"/> IN
+            <div className={`border d-flex my-auto mx-3 p-1  ${registrationLayoutSignUpIn}`} >
+              <p className="bg-dangerr text-center h4 m-auto" onClick={() => startAnimation()}>
+                  SIGN
+                  {
+                    pathname === "/sign-in"
+                    ? <BiSolidLeftArrow className="m-0 me-1 my-4" size="1em"/>
+                    : <BiSolidRightArrow className="m-0 me-1 my-4" size="1em"/>
+                  }
+                  {sign}
               </p>
-            </div>
+              </div>
             <div className={` bg-primary-subtlee my-auto ${registrationLayoutStick} `} ></div>
           </div>
-          <div className="bg-danger-subtle col-2 d-flex ms-auto" >
-            <img src={ballIcon} alt="the ball icon" width="80em" className="ms-auto" />
+          <div className="bg-danger d-none d-sm-block col-sm-1 col-md-2 col-lg-3 p-0 my-auto mx-0" >
+            <img src={ballIcon} alt="the ball icon" width="80em" className="bg-danger-subtle float-end img-fluid m-0" />
           </div>
         </div>
       </div>
