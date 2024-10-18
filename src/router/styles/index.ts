c5845f5aac22a79edaaaed11e85cae8ec32763a4
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-18 21:59:09 +0100

    update : refactor the logic of components

--- src/router/styles/index.ts
+++ src/router/styles/index.ts
@@ -3,3 +3,5 @@ import style from "@router/styles/RegistrationLayout.module.css"
 export const registrationLayout = style['registrationLayout']
 export const registrationLayoutStick = style['registrationLayoutStick']
 export const registrationLayoutSignUpIn = style['registrationLayoutSignUpIn']
+export const registrationLayoutAnimation = style['registrationLayoutAnimation']
+export const registrationLayoutStickAnimation = style['registrationLayoutStickAnimation']
