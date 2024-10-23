4c190ec15e9eaf755eb27ffa49e8a39cd3c89a18
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-23 13:56:27 +0100

    dashboard layout removing the gap between the sidebare and the outlet

--- src/router/layouts/DashboardLayout.tsx
+++ src/router/layouts/DashboardLayout.tsx
@@ -6,9 +6,9 @@ import { dashboardLayout } from "../styles";
 const DashboardLayout = () => {
   return (
     <Fragment>
-      <div className={`d-flex gap-2 bg-dangers ${dashboardLayout} `} style={{height: "100%"}}>
+      <div className={`d-flex gap-0 bg-dangers ${dashboardLayout} `} style={{height: "100%"}}>
         <Sidebar/>
-        <div className="p-2 w-100 h-auto overflow-y-auto overflow-x-hidden">
+        <div className="p-0 w-100 h-auto overflow-y-auto overflow-x-hidden">
           <Outlet />
         </div>
       </div>
