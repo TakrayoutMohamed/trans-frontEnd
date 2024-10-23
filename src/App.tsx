bb3c2320d0d3eb537a78094fc3d9e6915c415a54
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-23 12:58:08 +0100

    remove some console logs

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,13 @@ import { useSelector } from "react-redux";
 import Cookies from "js-cookie";
 import { setAccessToken } from "./states/authentication/accessTokenSlice";
 import { axiosPrivate } from "./services/api/axios";
-import useRefreshToken from "./services/hooks/useRefreshToken";
 
 function App() {
   const accessToken = useSelector(
     (state: RootState) => state.accessToken.value
   );
-  const refresh = useRefreshToken();
   const dispatch = store.dispatch;
-  console.log("accessToken from app.tsx {" + accessToken + "}");
   useLayoutEffect(() => {
-    console.log("accessToken from app.tsx {" + accessToken + "}");
-    console.log("access token : " + Cookies.get("accessToken"));
     if (Cookies.get("accessToken") && !accessToken)
     {
       const checkAuthentication = async () => {
