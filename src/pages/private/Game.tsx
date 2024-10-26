7b74755971427786e9bf2f7393b75c46aed809f2
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-26 14:23:05 +0100

    Create Profile In Game component in a step to separer the components of the Game

--- src/pages/private/Game.tsx
+++ src/pages/private/Game.tsx
@@ -1,6 +1,6 @@
-import { goldenMedalIcon, pongPlayer, profileIcon } from "@/media-exporting";
+import { pongPlayer } from "@/media-exporting";
 import BackgroundCircles from "@privateComponents/BackgroundCircles";
-import { RiMenuSearchLine } from "react-icons/ri";
+import ProfileInGame from "./components/ProfileInGame";
 
 const Game = () => {
   return (
@@ -43,57 +43,7 @@ const Game = () => {
                 </div>
               </div>
             </div>
-            <div
-              className="bg-secondary bg-opacity-10 col-12 col-sm-4 ms-auto border mt-3 rounded-4"
-              style={{ backdropFilter: "blur(4px)" }}
-            >
-              <div className="h6 text-center">My Profile</div>
-              <div className="row col-12 p-0 m-0 mt-2">
-                <div className="bg-infoo col-6 d-flex flex-row p-0 m-0 ">
-                  <img
-                    src={profileIcon}
-                    alt="my-profile"
-                    width="50em"
-                    height="50em"
-                    className="rounded-circle bg-success p-0 m-0"
-                  />
-                  <div className="d-flex flex-column m-0 ms-1 p-0">
-                    <div className="bg-successs me-auto fw-semibold">
-                      Alvares
-                    </div>
-                    <p className="p-0 m-0">
-                      <RiMenuSearchLine /> level 8.57
-                    </p>
-                  </div>
-                </div>
-                <div className="col-1 vr p-0 m-1 border border-2"></div>
-                <div className="bg-dangerr col-5 d-flex">
-                  <img
-                    src={goldenMedalIcon}
-                    alt="medal Icon"
-                    width="70em"
-                    className="m-0 p-0 m-auto"
-                  />
-                </div>
-              </div>
-              <hr className="m-2 my-4 p-0 border border-2" />
-              <div className="row mb-2 justify-content-evenly">
-                <div className="col-3 p-0 text-center">
-                  <div className="text-center h6 row m-0">last Game</div>
-                  <div className="text-danger pt-3">Lost</div>
-                </div>
-                <div className="vr col-1 p-0 mx-1 my-2 border border-2"></div>
-                <div className="col-4 p-0">
-                  <div className="text-center h6 row m-0">Status</div>
-                  <div className="text-success pt-3">online</div>
-                </div>
-                <div className="vr col-1 p-0 mx-1 my-2 border border-2"></div>
-                <div className="col-3 p-0 text-center">
-                  <div className="text-center h6 row m-0">Last Score</div>
-                  <div className="text-success pt-3">172.35 xp</div>
-                </div>
-              </div>
-            </div>
+            <ProfileInGame/>
           </div>
           {/* <div className="bg-success row m-0">
 						here is the second container
---
+++ src/pages/private/components/ProfileInGame.tsx
@@ -0,0 +1,60 @@
+import { goldenMedalIcon, profileIcon } from "@/media-exporting";
+import { RiMenuSearchLine } from "react-icons/ri";
+
+const ProfileInGame = () => {
+  return (
+    <>
+      <div
+        className="bg-secondary bg-opacity-10 col-12 col-sm-4 ms-auto border mt-3 rounded-4"
+        style={{ backdropFilter: "blur(4px)" }}
+      >
+        <div className="h6 text-center">My Profile</div>
+        <div className="row col-12 p-0 m-0 mt-2">
+          <div className="bg-infoo col-6 d-flex flex-row p-0 m-0 ">
+            <img
+              src={profileIcon}
+              alt="my-profile"
+              width="50em"
+              height="50em"
+              className="rounded-circle bg-success p-0 m-0"
+            />
+            <div className="d-flex flex-column m-0 ms-1 p-0">
+              <div className="bg-successs me-auto fw-semibold">Alvares</div>
+              <p className="p-0 m-0">
+                <RiMenuSearchLine /> level 8.57
+              </p>
+            </div>
+          </div>
+          <div className="col-1 vr p-0 m-1 border border-2"></div>
+          <div className="bg-dangerr col-5 d-flex">
+            <img
+              src={goldenMedalIcon}
+              alt="medal Icon"
+              width="70em"
+              className="m-0 p-0 m-auto"
+            />
+          </div>
+        </div>
+        <hr className="m-2 my-4 p-0 border border-2" />
+        <div className="row mb-2 justify-content-evenly">
+          <div className="col-3 p-0 text-center">
+            <div className="text-center h6 row m-0">last Game</div>
+            <div className="text-danger pt-3">Lost</div>
+          </div>
+          <div className="vr col-1 p-0 mx-1 my-2 border border-2"></div>
+          <div className="col-4 p-0 text-center">
+            <div className="h6 m-0">Status</div>
+            <div className="text-success pt-3">online</div>
+          </div>
+          <div className="vr col-1 p-0 mx-1 my-2 border border-2"></div>
+          <div className="col-3 p-0 text-center">
+            <div className="text-center h6 row m-0">Last Score</div>
+            <div className="text-success pt-3">172.35 xp</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProfileInGame;
