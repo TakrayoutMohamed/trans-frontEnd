18c2538d830d29f0b7dee83cec0d99db236c5011
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-28 14:18:09 +0100

    make the Recent In game renders only last 10 games

--- src/pages/private/components/RecentInGame.tsx
+++ src/pages/private/components/RecentInGame.tsx
@@ -25,6 +25,30 @@ const data = [
     player1: { name: "negredoooo3", scored: 9 },
     player2: { name: "alvaresssss3", scored: 6 },
   },
+  {
+    player1: { name: "alvares2", scored: 12 },
+    player2: { name: "negredo2", scored: 10 },
+  },
+  {
+    player1: { name: "negredoooo3", scored: 9 },
+    player2: { name: "alvaresssss3", scored: 6 },
+  },
+  {
+    player1: { name: "alvares2", scored: 12 },
+    player2: { name: "negredo2", scored: 10 },
+  },
+  {
+    player1: { name: "negredoooo3", scored: 9 },
+    player2: { name: "alvaresssss3", scored: 6 },
+  },
+  {
+    player1: { name: "alvares2", scored: 12 },
+    player2: { name: "negredo2", scored: 10 },
+  },
+  {
+    player1: { name: "negredoooo3", scored: 9 },
+    player2: { name: "alvaresssss3", scored: 6 },
+  },
 ];
 
 type PlayerInfoProps = {
@@ -71,12 +95,12 @@ interface GamesType {
   player1: {
     name: string;
     scored: number;
-  },
+  };
   player2: {
     name: string;
     scored: number;
-  }
-};
+  };
+}
 
 const RecentInGame = () => {
   let matches: GamesType[] | undefined = data;
@@ -86,28 +110,47 @@ const RecentInGame = () => {
       <div className="w-100 m-0">
         <div className="h3 text-center">Recent</div>
         <div className="d-flex flex-column rounded-5 border p-1 m-0">
-          {!matches || !matches.length && <div className="h4 text-warning"> No matches yet</div>}
-          {matches &&
-            matches?.map((match, index) => (
-              <div
-                key={index}
-                className="bg-primary bg-opacity-50 d-flex rounded-5 mb-1 p-1 justify-content-between"
-              >
-                {match.player1.scored >= match.player2.scored ? (
-                  <>
-                    <NameAndImageIcon player={match.player1} isWinner={true} />
-                    <div className="h3 m-auto">VS</div>
-                    <NameAndImageIcon player={match.player2} isWinner={false} />
-                  </>
-                ) : (
-                  <>
-                    <NameAndImageIcon player={match.player2} isWinner={true} />
-                    <div className="h3 m-auto">VS</div>
-                    <NameAndImageIcon player={match.player1} isWinner={false} />
-                  </>
-                )}
-              </div>
+          {!matches ||
+            (!matches.length && (
+              <div className="h4 text-warning"> No matches yet</div>
             ))}
+          {matches &&
+            matches?.map((match, index) =>
+              index < 10 ? (
+                <div
+                  key={index}
+                  className="bg-primary bg-opacity-50 d-flex rounded-5 mb-1 p-1 justify-content-between"
+                >
+                  {match.player1.scored >= match.player2.scored ? (
+                    <>
+                      <NameAndImageIcon
+                        player={match.player1}
+                        isWinner={true}
+                      />
+                      <div className="h3 m-auto">VS</div>
+                      <NameAndImageIcon
+                        player={match.player2}
+                        isWinner={false}
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <NameAndImageIcon
+                        player={match.player2}
+                        isWinner={true}
+                      />
+                      <div className="h3 m-auto">VS</div>
+                      <NameAndImageIcon
+                        player={match.player1}
+                        isWinner={false}
+                      />
+                    </>
+                  )}
+                </div>
+              ) : (
+                <span key={index}></span>
+              )
+            )}
         </div>
       </div>
     </>
