7b426de238be8f1077661e22f896a30d357cfd2b
TakrayoutMohamed <takrayoutm@gmail.com>
2024-10-28 12:38:47 +0100

    Update RecentInGame
    setting the type of the game data

--- src/pages/private/components/RecentInGame.tsx
+++ src/pages/private/components/RecentInGame.tsx
@@ -1,6 +1,6 @@
 import { profileIcon } from "@/media-exporting";
 
-const matches = [
+const data = [
   {
     player1: { name: "alvares", scored: 6 },
     player2: { name: "negredo", scored: 7 },
@@ -35,33 +35,31 @@ type PlayerInfoProps = {
   isWinner: boolean;
 };
 
-var count = 0;
-
 const NameAndImageIcon = ({ player, isWinner }: PlayerInfoProps) => {
-  count++;
-  console.log("NameAndImage rendered " + count);
   return isWinner ? (
     <div className="d-flex flex-row w-100">
       <img
         src={profileIcon}
         alt="playerIcon"
+        height="100%"
         width="40%"
         className="bg-success rounded-circle"
       />
-      <div className="fw-medium w-100 text-center my-auto">
+      <div className="fw-medium w-100 text-center my-auto" title={player.name}>
         {player.name.length > 8 && player.name.substring(0, 6).concat("...")}
         {player.name.length <= 8 && player.name}
       </div>
     </div>
   ) : (
     <div className="d-flex flex-row w-100">
-      <div className="fw-medium w-100 text-center my-auto ">
+      <div className="fw-medium w-100 text-center my-auto " title={player.name}>
         {player.name.length > 8 && player.name.substring(0, 6).concat("...")}
         {player.name.length <= 8 && player.name}
       </div>
       <img
         src={profileIcon}
         alt="playerIcon"
+        height="100%"
         width="40%"
         className="bg-danger rounded-circle"
       />
@@ -69,16 +67,32 @@ const NameAndImageIcon = ({ player, isWinner }: PlayerInfoProps) => {
   );
 };
 
+interface GamesType {
+  player1: {
+    name: string;
+    scored: number;
+  },
+  player2: {
+    name: string;
+    scored: number;
+  }
+};
+
 const RecentInGame = () => {
+  let matches: GamesType[] | undefined = data;
+
   return (
     <>
       <div className="w-100 m-0">
         <div className="h3 text-center">Recent</div>
         <div className="d-flex flex-column rounded-5 border p-1 m-0">
-          {!matches && <div className="h4 text-warning"> No matches yet</div>}
+          {!matches || !matches.length && <div className="h4 text-warning"> No matches yet</div>}
           {matches &&
-            matches.map((match, index) => (
-              <div key={index} className="bg-primary bg-opacity-50 d-flex rounded-5 mb-1 p-1 justify-content-between">
+            matches?.map((match, index) => (
+              <div
+                key={index}
+                className="bg-primary bg-opacity-50 d-flex rounded-5 mb-1 p-1 justify-content-between"
+              >
                 {match.player1.scored >= match.player2.scored ? (
                   <>
                     <NameAndImageIcon player={match.player1} isWinner={true} />
