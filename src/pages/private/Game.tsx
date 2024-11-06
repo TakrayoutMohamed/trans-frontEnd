8a67cee4083f2295bab04537168d4c2ae46baad3
TakrayoutMohamed <takrayoutm@gmail.com>
2024-11-06 07:45:12 +0100

    new update ....

--- src/pages/private/Game.tsx
+++ src/pages/private/Game.tsx
@@ -9,15 +9,15 @@ import { game } from "./styles";
 const Game = () => {
   return (
     <>
-      <div className={`position-relative w-100 h-100 m-0 ${game}`}>
+      <div className={`position-relative container-fluid w-100 m-0 ${game}`}>
         <div
-          className="position-absolute bg-transparent h-100 w-100 top-0 start-0 position-relative overflow-y-hidden"
+          className="position-absolute w-100 top-0 start-0 position-relative overflow-y-hidden" id="gameBackground" 
           style={{}}
         >
           <BackgroundCircles />
         </div>
-        <div className="bg-secondary-subtlee position-absolute container-fluid  text-white w-100 top-0 start-0 m-0">
-          <div className="bg-danger-subtlee d-flex flex-column flex-md-row m-0 mx-5">
+        <div className="bg-secondary-subtle mx-auto position-relative text-white top-0 start-0" id="gameContent">
+          <div className="d-flex flex-column flex-md-row m-0">
             <div className="bg-dangerr col-12 d-flex d-block d-md-none mb-5"></div>
             <div className="bg-dangere col-12 col-md-8 d-flex flex-row">
               <PongPlayerInGame />
@@ -29,7 +29,7 @@ const Game = () => {
               <ProfileInGame />
             </div>
           </div>
-          <div className="bg-successs row m-0 my-3 mx-5 ">
+          <div className="row m-0 ">
             <div className="bg-secondaryy col-12 col-sm-8 m-0 p-0">
               <div className="bg-dangerr d-flex flex-row">
                 <GameModeInGame />
