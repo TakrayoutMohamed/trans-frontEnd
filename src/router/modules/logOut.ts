850f3c472220cfdf0e906e60e13afad13fdb45ca
TakrayoutMohamed <takrayoutm@gmail.com>
2024-11-06 10:26:52 +0100

    Update logOut
    adding a log and a comment to notify me that it need to add the request to the server to log out

--- src/router/modules/logOut.ts
+++ src/router/modules/logOut.ts
@@ -1,16 +1,16 @@
-import { redirect } from "react-router-dom"
-import Cookies from "js-cookie"
+import { redirect } from "react-router-dom";
+import Cookies from "js-cookie";
 import { store } from "@/src/states/store";
 import { setUnauthenticated } from "@/src/states/authentication/authenticatorSlice";
 import { setAccessToken } from "@/src/states/authentication/accessTokenSlice";
 const logOut = () => {
-    const dispatch = store.dispatch;
-    Cookies.remove("accessToken");
-    dispatch(setAccessToken(undefined));
-    dispatch(setUnauthenticated());
-  return (
-    redirect("/sign-in")
-  )
-}
+  //here i need to send a request to backend to logout
+  console.log("here i need to send a request to backend to logout");
+  const dispatch = store.dispatch;
+  Cookies.remove("accessToken");
+  dispatch(setAccessToken(undefined));
+  dispatch(setUnauthenticated());
+  return redirect("/sign-in");
+};
 
-export default logOut
+export default logOut;
