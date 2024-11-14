0c387ad7f930ed1a9efa5389a67c72beb032be65
TakrayoutMohamed <takrayoutm@gmail.com>
2024-11-14 18:19:02 +0100

    Add : styles of the profile component is chat

--- src/router/styles/index.ts
+++ src/router/styles/index.ts
@@ -1,15 +1,18 @@
-import style from "@router/styles/RegistrationLayout.module.css"
+import style from "@router/styles/RegistrationLayout.module.css";
 import homeLayoutStyles from "@router/styles/HomeLayout.module.css";
 import DashboardLayoutStyles from "@router/styles/DashboardLayout.module.css";
-import ChatStyle from "@router/styles/ChatLayout.module.css"
+import ChatStyle from "@router/styles/ChatLayout.module.css";
+import ChatProfileStyles from "@router/styles/componentsStyles/ChatProfileStyles.module.css";
 
-export const homeLayout = homeLayoutStyles['homeLayout']
+export const homeLayout = homeLayoutStyles["homeLayout"];
 
-export const dashboardLayout = DashboardLayoutStyles['dashboardLayout'];
+export const dashboardLayout = DashboardLayoutStyles["dashboardLayout"];
 
-export const registrationLayout = style['registrationLayout']
-export const registrationLayoutStick = style['registrationLayoutStick']
-export const registrationLayoutSignUpIn = style['registrationLayoutSignUpIn']
-export const registrationLayoutAnimation = style['registrationLayoutAnimation']
-export const registrationLayoutStickAnimation = style['registrationLayoutStickAnimation']
-export const chatLayout = ChatStyle['chatLayout']
+export const registrationLayout = style["registrationLayout"];
+export const registrationLayoutStick = style["registrationLayoutStick"];
+export const registrationLayoutSignUpIn = style["registrationLayoutSignUpIn"];
+export const registrationLayoutAnimation = style["registrationLayoutAnimation"];
+export const registrationLayoutStickAnimation =
+  style["registrationLayoutStickAnimation"];
+export const chatLayout = ChatStyle["chatLayout"];
+export const chatProfileStyles = ChatProfileStyles["chatProfileStyles"];
