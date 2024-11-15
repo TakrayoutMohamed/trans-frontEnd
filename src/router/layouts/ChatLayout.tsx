fee738229de8cc4ea849586a87b93d66c5da7838
TakrayoutMohamed <takrayoutm@gmail.com>
2024-11-15 17:30:20 +0100

    Updating styles and Profile in chat

--- src/router/layouts/ChatLayout.tsx
+++ src/router/layouts/ChatLayout.tsx
@@ -7,13 +7,13 @@ import "@router/styles/chatGlobalOverridingStyles.css";
 import Profile from "./components/chat/Profile";
 
 const ChatLayout = () => {
-  const [isProfileVisible, setProfileVisible] = useState<boolean>(true);
+  const [isProfileVisible, setProfileVisible] = useState<boolean>(false);
   console.log("chat layout reloaded");
   return (
     <Fragment>
       <div className={`${chatLayout}`}>
         <div className="bg-dangerr d-none d-sm-block p-5"></div>
-        <main className="" id="main">
+        <main className="bg-infos" id="main">
           <section className="section1 d-n" id="section1">
             {/* the component of the chat previous conversations */}
             <ConversationsList />
@@ -22,7 +22,7 @@ const ChatLayout = () => {
             {/* the component of the chat content */}
             <Outlet context={setProfileVisible} />
           </section>
-          <section className={``} id="section2">
+          <section className={`${!isProfileVisible && "d-none"} `} id="section2">
             <Profile isProfileVisible={isProfileVisible} />
           </section>
         </main>
