6cf06e66166637501b9d6c3f5e97177eb0bb9291
TakrayoutMohamed <takrayoutm@gmail.com>
2024-11-17 19:56:53 +0100

    Update : adding grid layout
    making the grade layout for the profilelayout

--- src/router/layouts/ProfileLayout.tsx
+++ src/router/layouts/ProfileLayout.tsx
@@ -1,15 +1,37 @@
-import { Fragment } from 'react'
-import { profileLayout } from '../styles'
-import { Outlet } from 'react-router-dom'
+import { Fragment } from "react";
+import { profileLayout } from "../styles";
+import { Outlet } from "react-router-dom";
+import Stats, { statsType } from "./components/profile/Stats";
 
 const ProfileLayout = () => {
   return (
-	<Fragment>
-		<div className={`${profileLayout}`}>
-		<Outlet/>
-		</div>
-	</Fragment>
-  )
-}
+    <Fragment>
+      <div className={`${profileLayout}`}>
+        <div className="aiStats">
+          <Stats title={"AI STATS"} statsType={statsType.AISTATS}></Stats>
+        </div>
+        <div className="classicTournamentStats">
+          <div className="classicStats">
+            <Stats
+              title={"CLASSIC STATS"}
+              statsType={statsType.CLASSICSTATS}
+            ></Stats>
+          </div>
+          <div className="tournamentStats">
+            <Stats
+              title={"TOURNAMENT STATS"}
+              statsType={statsType.TOURNAMENTSTATS}
+            ></Stats>
+          </div>
+        </div>
+        <div className="profileStats">
+          <Outlet />
+        </div>
+        <div className="breadcrumbs"></div>
+        <div className="waletStats"></div>
+      </div>
+    </Fragment>
+  );
+};
 
-export default ProfileLayout
+export default ProfileLayout;
