76b42f4d03b295756852dcb1f6f2f1ec86c8f379
TakrayoutMohamed <takrayoutm@gmail.com>
2024-11-19 22:04:27 +0100

    Update profile styling

--- src/pages/private/Profile.tsx
+++ src/pages/private/Profile.tsx
@@ -2,7 +2,14 @@ import { profile } from "./styles"
 
 const Profile = () => {
   return (
-    <div className={`${profile}`}>Profile</div>
+    <div className={`${profile}`}>
+      
+      <div className="">Player Name : AbdEllatif Elfany</div>
+      <div className="">Player Level : lvl.15.21</div>
+      <div className="">Started On : 19 oct 2022</div>
+      <div className="">World Ranking : 12</div>
+      <div className="">Player Id : 000000124</div>
+    </div>
   )
 }
 
