423bc3e73338f5fc91acf2c5bf853960f11778b4
TakrayoutMohamed <takrayoutm@gmail.com>
2024-11-19 20:49:03 +0100

    profile layout update its styles

--- src/router/layouts/ProfileLayout.tsx
+++ src/router/layouts/ProfileLayout.tsx
@@ -1,7 +1,8 @@
 import { Fragment } from "react";
 import { profileLayout } from "../styles";
-import { Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 import Stats from "./components/profile/Stats";
+import { profileIcon } from "@/media-exporting";
 
 enum statsType {
   AISTATS,
@@ -14,25 +15,49 @@ const ProfileLayout = () => {
     <Fragment>
       <div className={`${profileLayout}`}>
         <div className="aiStats">
-          <Stats title={"AI STATS"} statsType={statsType.AISTATS}></Stats>
+          <Stats title={"AI STATS"} statsType={statsType.AISTATS} />
         </div>
         <div className="classicTournamentStats">
           <div className="classicStats">
-            <Stats
-              title={"CLASSIC STATS"}
-              statsType={statsType.CLASSICSTATS}
-            ></Stats>
+            <Stats title={"CLASSIC STATS"} statsType={statsType.CLASSICSTATS} />
           </div>
-          {/* <div className="" style={{minHeight:"20em"}} ></div> */}
           <div className="tournamentStats">
             <Stats
               title={"TOURNAMENT STATS"}
               statsType={statsType.TOURNAMENTSTATS}
-            ></Stats>
+            />
           </div>
         </div>
-        <div className="profileStats">
-          <Outlet />
+        <div className="profileStatsLayout">
+          <div className="profile-side-bar">
+            <NavLink className="" to="recent">
+              Recent
+            </NavLink>
+            <NavLink className="" to="me" >
+              Profile
+            </NavLink>
+            <NavLink className="" to="friends">
+              Friends
+            </NavLink>
+          </div>
+          <div className="user-image-link-content">
+            <div className="user-image">
+              <svg className="">
+                <image
+                  width="100%"
+                  height="100%"
+                  href="/assets/images/profileBackgroundCurve.svg"
+                  className="mt-autos"
+                />
+              </svg>
+              <div className="">
+                <img src={profileIcon} alt="user image" />
+              </div>
+            </div>
+            <div className="link-content">
+              <Outlet />
+            </div>
+          </div>
         </div>
         <div className="breadcrumbs"></div>
         <div className="waletStats"></div>
