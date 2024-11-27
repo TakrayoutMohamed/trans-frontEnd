bb3949b890212ee6cb921146fca4746f5ebacc06
TakrayoutMohamed <takrayoutm@gmail.com>
2024-11-27 16:28:43 +0100

    Adding the background image for profile component

--- media-exporting.ts
+++ media-exporting.ts
@@ -32,6 +32,7 @@ export const robot = "/assets/images/robot.svg";
 export const friends = "/assets/images/friends.svg";
 export const tournament = "/assets/images/tournament.svg";
 export const chatBackgroundImage = "/assets/images/chatBackgroundImage.svg";
+export const profileBackgroundImage = "/assets/images/profileBackgroundImage.svg";
 export const coinsBackground = "/assets/images/coinsBackground.svg";
 export const settingBackgroundImage =
   "/assets/images/settingBackgroundImage.png";
