64ad43d19f93b56d55b1cc556a198d105e5f0ee7
TakrayoutMohamed <takrayoutm@gmail.com>
2024-11-27 14:39:03 +0100

    Update : adding the functions needed  to password hidding and showing to work properly

--- src/pages/private/SettingPassword.tsx
+++ src/pages/private/SettingPassword.tsx
@@ -1,7 +1,58 @@
-import { IoMdEyeOff } from "react-icons/io";
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 import { settingPassword } from "./styles";
 
+function toggleCurrentPasswordEye() {
+  let currentPasswordOn = document.getElementById("toggleCurrentPasswordEyeOn");
+  let currentPasswordInput = document.getElementById("current-password");
+  const type =
+    currentPasswordInput?.getAttribute("type") === "password"
+      ? "text"
+      : "password";
+  currentPasswordOn?.classList.contains("d-none")
+    ? currentPasswordOn.classList.remove("d-none")
+    : currentPasswordOn?.classList?.add("d-none");
+
+  let currentPasswordOff = document.getElementById(
+    "toggleCurrentPasswordEyeOff"
+  );
+  currentPasswordOff?.classList.contains("d-none")
+    ? currentPasswordOff.classList.remove("d-none")
+    : currentPasswordOff?.classList?.add("d-none");
+  currentPasswordInput?.setAttribute("type", type);
+}
+function toggleNewPasswordEye() {
+  let newPasswordOn = document.getElementById("toggleNewPasswordEyeOn");
+  let newPasswordInput = document.getElementById("new-password");
+  let newPasswordOff = document.getElementById("toggleNewPasswordEyeOff");
+  const type =
+    newPasswordInput?.getAttribute("type") === "password" ? "text" : "password";
+  newPasswordOn?.classList.contains("d-none")
+    ? newPasswordOn.classList.remove("d-none")
+    : newPasswordOn?.classList?.add("d-none");
+  newPasswordOff?.classList.contains("d-none")
+    ? newPasswordOff.classList.remove("d-none")
+    : newPasswordOff?.classList?.add("d-none");
+  newPasswordInput?.setAttribute("type", type);
+}
+function toggleRepeatPasswordEye() {
+  let repeatPasswordOn = document.getElementById("toggleRepeatPasswordEyeOn");
+  let repeatPasswordInput = document.getElementById("repeat-password");
+  let repeatPasswordOff = document.getElementById("toggleRepeatPasswordEyeOff");
+  const type =
+    repeatPasswordInput?.getAttribute("type") === "password"
+      ? "text"
+      : "password";
+  repeatPasswordOn?.classList.contains("d-none")
+    ? repeatPasswordOn.classList.remove("d-none")
+    : repeatPasswordOn?.classList?.add("d-none");
+  repeatPasswordOff?.classList.contains("d-none")
+    ? repeatPasswordOff.classList.remove("d-none")
+    : repeatPasswordOff?.classList?.add("d-none");
+  repeatPasswordInput?.setAttribute("type", type);
+}
+
 const SettingPassword = () => {
+  console.log("component of settings password is re-rendered");
   return (
     <div className={settingPassword}>
       <form
@@ -23,8 +74,16 @@ const SettingPassword = () => {
               <IoMdEyeOff
                 color="white"
                 size={20}
-                id="toggleCurrentPassword"
+                id="toggleCurrentPasswordEyeOff"
                 className="current-password-eye"
+                onClick={toggleCurrentPasswordEye}
+              />
+              <IoMdEye
+                color="white"
+                size={20}
+                id="toggleCurrentPasswordEyeOn"
+                className="new-password-eye d-none"
+                onClick={toggleCurrentPasswordEye}
               />
             </span>
           </div>
@@ -42,8 +101,16 @@ const SettingPassword = () => {
               <IoMdEyeOff
                 color="white"
                 size={20}
-                id="toggleNewPassword"
+                id="toggleNewPasswordEyeOff"
                 className="new-password-eye"
+                onClick={toggleNewPasswordEye}
+              />
+              <IoMdEye
+                color="white"
+                size={20}
+                id="toggleNewPasswordEyeOn"
+                className="new-password-eye d-none"
+                onClick={toggleNewPasswordEye}
               />
             </span>
           </div>
@@ -65,8 +132,16 @@ const SettingPassword = () => {
               <IoMdEyeOff
                 color="white"
                 size={20}
-                id="toggleRepeatPassword"
+                id="toggleRepeatPasswordEyeOff"
                 className="repeat-password-eye"
+                onClick={toggleRepeatPasswordEye}
+              />
+              <IoMdEye
+                color="white"
+                size={20}
+                id="toggleRepeatPasswordEyeOn"
+                className="repeat-password-eye d-none"
+                onClick={toggleRepeatPasswordEye}
               />
             </span>
           </div>
