e3115d546565d518c0824799a60c2ad9e733d68d
TakrayoutMohamed <takrayoutm@gmail.com>
2024-11-29 22:14:50 +0100

    update finishing the profile in game and  update the position of pong player

--- src/pages/private/components/gameComponents/ProfileInGame.tsx
+++ src/pages/private/components/gameComponents/ProfileInGame.tsx
@@ -5,52 +5,40 @@ import { gameProfileInGame } from "../../styles";
 const ProfileInGame = () => {
   return (
     <>
-      <div className={`bg-successs d-flex flex-column ${gameProfileInGame}`} >
-        <div className=" text-center">My Profile</div>
-        <div className="row col-12 p-0 m-0 mt-2">
-          <div className="bg-infoo col-6 d-flex flex-row flex-wrap p-0 m-0 ">
-            <div className="col-6 d-flex p-0 m-0">
-              <img
-                src={profileIcon}
-                alt="my-profile"
-                width="80%"
-                className="rounded-circle bg-success p-0 m-auto"
-                style={{maxWidth:"100%", height:"auto"}}
-              />
+      <div className={gameProfileInGame}>
+        <div className="profile-title">My Profile</div>
+        <div className="medal-user">
+          <div className="user-image-name-level">
+            <div className="user-image">
+              <img src={profileIcon} alt="my-profile" className="" />
             </div>
-            <div className="bg-infoo text-black d-flex flex-column m-0 p-0 my-auto ">
-              <div className="bg-successs me-auto fw-semibold">Alvares</div>
-              <p className="p-0 m-0">
+            <div className="user-name-level">
+              <div className="user-name">Alvares</div>
+              <p className="user-level">
                 <RiMenuSearchLine /> level 8.57
               </p>
             </div>
           </div>
-          <div className="col-1 vr p-0 m-0 border border-2"></div>
-          <div className="bg-danger-subtlee col-5 d-flex p-0 mx-auto">
-            <img
-              src={goldenMedalIcon}
-              alt="medal Icon"
-              width="70%"
-              className="bg-dangerr m-0 p-0 mx-auto"
-              style={{maxWidth:"100%", height:"auto"}}
-            />
+          <div className="vertical-line"></div>
+          <div className="medal-image">
+            <img src={goldenMedalIcon} alt="medal Icon" className="" />
           </div>
         </div>
-        <hr className="m-2 p-0 border border-2" />
-        <div className="container w-100 row m-0 p-0 mb-2 justify-content-evenly mx-auto mt-auto">
-          <div className="col-3 p-0 text-center d-flex flex-column">
-            <div className="text-center h6 m-0">last Game</div>
-            <div className="text-danger mt-auto">Lost</div>
+        <hr className="horizontal-line" />
+        <div className="last-game-status-last-score">
+          <div className="last-game">
+            <div className="title">last Game</div>
+            <div className="text-danger content">Lost</div>
           </div>
-          <div className="vr col-1 p-0 mx-1 my-2 border border-2"></div>
-          <div className="col-3 p-0 text-center d-flex flex-column">
-            <div className="h6 m-0">Status</div>
-            <div className="text-success mt-auto">online</div>
+          <div className=" vertical-line"></div>
+          <div className="status">
+            <div className="title">Status</div>
+            <div className="text-success content">online</div>
           </div>
-          <div className="vr col-1 p-0 mx-1 my-2 border border-2"></div>
-          <div className="col-4 p-0 text-center d-flex flex-column">
-            <div className="text-center h6 m-0">Last Score</div>
-            <div className="text-success mt-auto">172.35xp</div>
+          <div className="vertical-line"></div>
+          <div className="last-score">
+            <div className="title">Last Score</div>
+            <div className="text-success content">172.35xp</div>
           </div>
         </div>
       </div>
