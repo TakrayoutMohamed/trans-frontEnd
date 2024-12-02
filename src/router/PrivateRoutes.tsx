295f631f52d813d34355e3835b4a2d0c7b6ee047
TakrayoutMohamed <takrayoutm@gmail.com>
2024-12-02 20:50:03 +0100

    Update: set the last location in Navigation state so the user can redirect to his previous page whene redirected to login page

--- src/router/PrivateRoutes.tsx
+++ src/router/PrivateRoutes.tsx
@@ -1,9 +1,10 @@
 import { RootState } from "@states/store";
 import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 const PrivateRoutes = () => {
   const isAuthenticated = useSelector((state: RootState) => state.authenticator.value);
-  return (isAuthenticated ? <Outlet/> : <Navigate to="sign-in" />);
+  const location = useLocation();
+  return (isAuthenticated ? <Outlet/> : <Navigate state={{from : location}} to="sign-in" />);
 };
 export default PrivateRoutes;
