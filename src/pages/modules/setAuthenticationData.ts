64b97b6fa61b0009ce73b758afec49f3c72d2742
TakrayoutMohamed <takrayoutm@gmail.com>
2024-12-07 17:52:49 +0100

    adding the function to set the authentication of user and its data to a module to be used

--- src/pages/modules/setAuthenticationData.ts
+++ src/pages/modules/setAuthenticationData.ts
@@ -1,13 +1,20 @@
 import Cookies from "js-cookie";
-import { setAuthenticated } from "@/src/states/authentication/authenticatorSlice";
+import {
+  setAuthenticated,
+  setUnauthenticated,
+} from "@/src/states/authentication/authenticatorSlice";
 import { setAccessToken } from "@/src/states/authentication/accessTokenSlice";
+import { setUser } from "@/src/states/authentication/userSlice";
 import { store } from "@/src/states/store";
+import { UserDataType } from "@/src/states/authentication/userSlice";
 
 const setCookies = (cookiesAccessToken: string): void => {
   Cookies.set("accessToken", cookiesAccessToken);
 };
-export default function (RespondedAccessToken: string): boolean {
-  const dispatch = store.dispatch;
+const dispatch = store.dispatch;
+export default function setAuthenticatedData(
+  RespondedAccessToken: string
+): boolean {
   setCookies(RespondedAccessToken);
   dispatch(setAccessToken(RespondedAccessToken));
   if (RespondedAccessToken) {
@@ -15,4 +22,16 @@ export default function (RespondedAccessToken: string): boolean {
     return true;
   }
   return false;
+}
+export function setUnAuthenticatedData() {
+  dispatch(setUnauthenticated());
+  dispatch(setAccessToken(""));
+  Cookies.remove("accessToken");
+}
+
+export function setUserData(userData: UserDataType){
+  dispatch(setUser(userData))
+  console.log("users data in set User data ")
+  console.log(userData);
+  
 }
