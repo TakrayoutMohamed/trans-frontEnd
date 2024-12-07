ddfacee27a3a488e51fdafa1fc7e1001e9dc6f95
TakrayoutMohamed <takrayoutm@gmail.com>
2024-12-07 18:30:41 +0100

    update useRefreshToken : remove some logs

--- src/services/hooks/useRefreshToken.tsx
+++ src/services/hooks/useRefreshToken.tsx
@@ -1,16 +1,10 @@
-import { store } from "@/src/states/store";
 import axios from "../api/axios";
-import { setAccessToken } from "@/src/states/authentication/accessTokenSlice";
 
 const refreshUrl = "refresh_token";
 
 const useRefreshToken = () => {
-  const dispatch = store.dispatch;
   const refresh = async () => {
     const response = await axios.post(refreshUrl);
-    console.log(`the access token is ${response.data.access_token}`);
-    console.log(response);
-    dispatch(setAccessToken(response?.data?.access_token));
     return response.data.access_token;
   };
   return refresh;
