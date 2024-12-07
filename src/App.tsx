c8ad57934976ba57ffda90dd433f42a87de87acf
TakrayoutMohamed <takrayoutm@gmail.com>
2024-12-07 17:56:28 +0100

    Update setting the data of the user after his login

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,49 @@
 import MainRoutingComponent from "@router/MainRoutingComponent.tsx";
-import { RootState, store } from "./states/store";
-import {
-  setAuthenticated,
-  setUnauthenticated,
-} from "./states/authentication/authenticatorSlice";
+import { RootState } from "./states/store";
 import { useLayoutEffect } from "react";
 import { useSelector } from "react-redux";
-import Cookies from "js-cookie";
-import { setAccessToken } from "./states/authentication/accessTokenSlice";
 import useAxiosPrivate from "./services/hooks/useAxiosPrivate";
-import axios from "./services/api/axios";
-
+import setAuthenticationData, {
+  setUnAuthenticatedData,
+  setUserData,
+} from "./pages/modules/setAuthenticationData";
 
 function App() {
+  const isAuthenticated = useSelector(
+    (state: RootState) => state.authenticator.value
+  );
   const accessToken = useSelector(
     (state: RootState) => state.accessToken.value
   );
   const axiosPrivateHook = useAxiosPrivate();
-  const dispatch = store.dispatch;
   useLayoutEffect(() => {
-    // if ( !Cookies.get("accessToken")) {
-      const checkAuthentication = async () => {
-        // send to server to verify if its a realy acceptable token
-        const res = await axiosPrivateHook.post("refresh_token");
-        return res;
-      };
-      checkAuthentication()
+    const checkAuthentication = async () => {
+      const res = await axiosPrivateHook.post("refresh_token");
+      return res;
+    };
+    const getUsersInfo = async () => {
+      try {
+        const userData = await axiosPrivateHook.get("user_info");
+        setUserData(userData.data);
+      } catch (err) {
+        console.log("error in getUsersInfo");
+        console.log(err);
+      }
+    };
+    if (isAuthenticated && accessToken) getUsersInfo();
+    checkAuthentication()
       .then((res) => {
-        // console.log("response from app : first one");
-        console.log(res);
-        // if the returned token is valid than store it in the accessToken state
         if (res.statusText == "OK") {
-          dispatch(setAccessToken(res.data.access_token));
-          Cookies.set("accessToken", res.data.access_token);
-          dispatch(setAuthenticated());
+          setAuthenticationData(res.data.access_token);
         } else throw Error("not valid access token alvares");
         return res.data;
       })
-      .catch(
-        // else remove that token
-        (err) => {
-          console.log("error from catch");
-          console.log(err);
-          dispatch(setUnauthenticated());
-          Cookies.remove("accessToken");
-        }
-      );
-      
-      // }
-      const getUsersInfo = async () => {
-        try{
-          const userData = await axiosPrivateHook.get("user_info");
-          console.log("responsegetUsersInfo");
-          console.log(userData)
-        }
-        catch(err){
-          console.log("error in getUsersInfo");
-          console.log(err)
-        }
-      }
-      getUsersInfo();
-  }, []);
+      .catch((err) => {
+        setUnAuthenticatedData();
+        console.log("error from catch");
+        console.log(err);
+      });
+  }, [isAuthenticated]);
   return (
     <>
       <MainRoutingComponent></MainRoutingComponent>
