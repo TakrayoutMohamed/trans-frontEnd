eaf6240d3855f3e57e88c3dbf9a947cd480c10e8
TakrayoutMohamed <takrayoutm@gmail.com>
2024-12-11 22:03:03 +0100

    test connecting to notifications

--- src/pages/public/Contact.tsx
+++ src/pages/public/Contact.tsx
@@ -1,7 +1,45 @@
+import { w3cwebsocket } from "websocket";
+import { useSelector } from "react-redux";
+import { RootState } from "@/src/states/store";
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const AccessToken = useSelector(
+    (state: RootState) => state.accessToken.value
+  );
+  const userData = useSelector(
+    (state: RootState) => state.user.value
+  );
+  const client = new w3cwebsocket(`ws://10.12.8.14:8000/ws/notification/?token=${AccessToken}`);
+  client.onopen = () => {
+    console.log("hello client connected");
+    // console.log(data.textMessage);
+  };
+  // socket.emit("message")
+  // console.log(message);
+  client.onmessage = (data) => {
+    console.log("this message recieved in client side")
+    console.log(data)
+  }
+  client.onclose = () => {
+    console.log("hello client disconnected");
+    // console.log(data.textMessage);
+  };
 
-export default Contact
+  const sendMessage = () => {
+    const data = {
+      "Sender" : userData.username,
+      "Reciever" : "receiverUserName",
+      "message" : `hello this is a message from the user[${userData.username}]`,
+    }
+    client.send(JSON.stringify(data))
+    console.log(data)
+  }
+
+  return <div>
+    <div className="btn btn-info" onClick={sendMessage} > send Message </div>
+    Contact
+
+
+  </div>;
+};
+
+export default Contact;
