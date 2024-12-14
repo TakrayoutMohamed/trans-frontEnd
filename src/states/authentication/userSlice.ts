eb1756cbd517d713f54e951383692030bc491250
TakrayoutMohamed <takrayoutm@gmail.com>
2024-12-14 10:29:03 +0100

    Update : the format of the user infos

--- src/states/authentication/userSlice.ts
+++ src/states/authentication/userSlice.ts
@@ -1,43 +1,39 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 export type UserDataType = {
-    email?: string;
-    first_name?: string;
-    last_name?: string;
-    username?: string;
-    avatar?: string;
-    bio?:string;
-    is_valid?: boolean;
-    is_active?: boolean;
-    is_staff?: boolean;
-    is_superuser?:boolean;
-    pyotp_secret?:boolean;
-    created_at?: string,
-    last_login?: string,
-    wins?: number,
-    losses?: number,
-    draws?: number,
-    matches_played?: number,
-    is2fa?: boolean
-}
+  email?: string;
+  first_name?: string;
+  last_name?: string;
+  username?: string;
+  avatar?: string;
+  created_at?: string;
+  last_login?: string;
+  wins?: number;
+  losses?: number;
+  draws?: number;
+  matches_played?: number;
+  is2fa?: boolean;
+  is_online?: boolean;
+  rank?: number;
+  level?:number;
+};
 
 interface AuthenticatorState {
-    value: UserDataType,
-
+  value: UserDataType;
 }
 
-const initialState: AuthenticatorState = {value: {}};
+const initialState: AuthenticatorState = { value: {} };
 
 const userSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {
-        setUser: (state, action: PayloadAction<UserDataType>) => {
-            state.value = action.payload;
-        }
-    }
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<UserDataType>) => {
+      state.value = action.payload;
+    },
+  },
 });
 
-export const {setUser} = userSlice.actions;
+export const { setUser } = userSlice.actions;
 
 export default userSlice.reducer;
