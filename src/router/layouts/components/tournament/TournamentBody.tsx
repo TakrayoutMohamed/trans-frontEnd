dca81f6be280321dd6601a09589828900c277ab2
TakrayoutMohamed <takrayoutm@gmail.com>
2024-12-17 23:00:46 +0100

    Update : not done Setting the data of the friend list

--- src/router/layouts/components/tournament/TournamentBody.tsx
+++ src/router/layouts/components/tournament/TournamentBody.tsx
@@ -1,11 +1,32 @@
+import { useSelector } from 'react-redux';
 import TournamentBodyLeftSide from './TournamentBodyLeftSide'
 import TournamentBodyMiddleSide from './TournamentBodyMiddleSide'
 import TournamentBodyRightSide from './TournamentBodyRightSide'
+import { RootState } from '@/src/states/store';
+import { useEffect, useState } from 'react';
+import { UserDataType } from '@/src/states/authentication/userSlice';
+
+interface FriendsDataType{
+	joined: boolean;
+	UserData : UserDataType
+}
 
 const TournamentBody = () => {
+	const friendsDataGlobal = useSelector((state: RootState) => state.friends.value);
+	const [friendsData, setFriendsData] = useState<FriendsDataType[]>([]);
+	useEffect(() => {
+		if (!friendsData)
+			setFriendsData(friendsDataGlobal);
+		else{
+			setFriendsData(friendsDataGlobal);
+		}
+		friendsData.map((friend) => { return {...friend, joined: !friend?.joined ? false : true}})
+	}, [friendsDataGlobal])
+	console.log("")
+
 	return (
 		<div className='TournamentBody'>
-			<TournamentBodyLeftSide />
+			<TournamentBodyLeftSide/>
 			<TournamentBodyMiddleSide />
 			<TournamentBodyRightSide />
 		</div>
