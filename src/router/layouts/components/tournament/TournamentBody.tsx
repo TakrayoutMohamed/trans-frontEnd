bdf6b2640238f3c529525892f918cf21daa3c58b
TakrayoutMohamed <takrayoutm@gmail.com>
2024-12-18 11:56:35 +0100

    Update : Fix the problem of cleaning data of friends object after adding new friend

--- src/router/layouts/components/tournament/TournamentBody.tsx
+++ src/router/layouts/components/tournament/TournamentBody.tsx
@@ -1,36 +1,58 @@
-import { useSelector } from 'react-redux';
-import TournamentBodyLeftSide from './TournamentBodyLeftSide'
-import TournamentBodyMiddleSide from './TournamentBodyMiddleSide'
-import TournamentBodyRightSide from './TournamentBodyRightSide'
-import { RootState } from '@/src/states/store';
-import { useEffect, useState } from 'react';
-import { UserDataType } from '@/src/states/authentication/userSlice';
+import { useSelector } from "react-redux";
+import TournamentBodyLeftSide from "./TournamentBodyLeftSide";
+import TournamentBodyMiddleSide from "./TournamentBodyMiddleSide";
+import TournamentBodyRightSide from "./TournamentBodyRightSide";
+import { RootState } from "@/src/states/store";
+import { useEffect, useState } from "react";
+import { UserDataType } from "@/src/states/authentication/userSlice";
 
-interface FriendsDataType{
-	joined: boolean;
-	UserData : UserDataType
+interface FriendsDataType extends UserDataType {
+  joined: boolean;
 }
 
 const TournamentBody = () => {
-	const friendsDataGlobal = useSelector((state: RootState) => state.friends.value);
-	const [friendsData, setFriendsData] = useState<FriendsDataType[]>([]);
-	useEffect(() => {
-		if (!friendsData)
-			setFriendsData(friendsDataGlobal);
-		else{
-			setFriendsData(friendsDataGlobal);
-		}
-		friendsData.map((friend) => { return {...friend, joined: !friend?.joined ? false : true}})
-	}, [friendsDataGlobal])
-	console.log("")
+  const friendsDataGlobal = useSelector(
+    (state: RootState) => state.friends.value
+  );
+  const [friendsData, setFriendsData] = useState<FriendsDataType[]>([]);
+  useEffect(() => {
+    if (!friendsData || !friendsData.length)
+      setFriendsData(
+        friendsDataGlobal.map((friend) => {
+          return { ...friend, joined: false };
+        })
+      );
+    else {
+      setFriendsData(
+        friendsDataGlobal.map((friend) => {
+          return {
+            ...friend,
+            joined: friendsData.find((object) => {
+              return object.username === friend.username;
+            })?.joined
+              ? true
+              : false,
+          };
+        })
+      );
+    }
+    friendsData.map((friend) => {
+      return { ...friend, joined: !friend?.joined ? false : true };
+    });
+  }, [friendsDataGlobal]);
+  console.log("");
 
-	return (
-		<div className='TournamentBody'>
-			<TournamentBodyLeftSide/>
-			<TournamentBodyMiddleSide />
-			<TournamentBodyRightSide />
-		</div>
-	)
-}
+  {
+    /* JEFF TODO : pass friendsData to PlayerHolder's and display the friends!!!! PS : we need a dummy friend data */
+  }
+
+  return (
+    <div className="TournamentBody">
+      <TournamentBodyLeftSide />
+      <TournamentBodyMiddleSide />
+      <TournamentBodyRightSide />
+    </div>
+  );
+};
 
 export default TournamentBody;
