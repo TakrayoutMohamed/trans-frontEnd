28341fce0bef3264c5d6a23c247332e1028f7a9d
TakrayoutMohamed <takrayoutm@gmail.com>
2024-12-21 22:58:49 +0100

    update : notification styles and also add and export a function for the progress bar for use if needed

--- src/router/layouts/components/notifications/NotificationsComponent.tsx
+++ src/router/layouts/components/notifications/NotificationsComponent.tsx
@@ -1,21 +1,74 @@
 import { notificationsComponent } from "@/src/router/styles";
+import { useEffect, useState } from "react";
 import { FaCheck } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 
-interface NotificationsComponentProps{
+interface ProgressBarProps {
+  toCloseAt?: number | false;
+  isPaused?: boolean;
+  startTime?: number;
+  onClose?: ((reason?: string | boolean | undefined) => void) | undefined;
+}
+
+export const ProgressBar = ({
+  toCloseAt,
+  isPaused,
+  startTime = 0,
+  onClose,
+}: ProgressBarProps) => {
+  const [progressBar, setProgressBar] = useState<number>(0);
+  // console.log(startTime + "    " + toCloseAt);
+
+  useEffect(() => {
+    if (toCloseAt && !isPaused) {
+      const timeId = setTimeout(() => {
+        const elapsed = Date.now() - startTime;
+        const remaining = toCloseAt - elapsed - 10;
+        setProgressBar(Math.max(remaining, 0));
+        if (remaining <= 0 || onClose) {
+          setProgressBar(0);
+          clearTimeout(timeId);
+        }
+      }, 50);
+    }
+  });
+  return (
+    <>
+      {toCloseAt && progressBar > 0 ? (
+        <progress value={(progressBar * 100) / toCloseAt} max={100}></progress>
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
+
+interface NotificationsComponentProps {
   message: string;
   reject: () => void;
   accept: () => void;
+  onClose?: ((reason?: string | boolean | undefined) => void) | undefined;
 }
 
-const NotificationsComponent = ({message, reject, accept} : NotificationsComponentProps) => {
+const NotificationsComponent = ({
+  message,
+  reject,
+  accept,
+}: NotificationsComponentProps) => {
+  console.log("Notification Component");
   return (
     <>
       <div className={notificationsComponent}>
-        <div className="notification-message">{message}</div>
-        <div className="notification-reject-accept">
-          <div className="notification-reject" onClick={reject}><ImCross color="red"/></div>
-          <div className="notification-accept" onClick={accept}><FaCheck color="greenyellow"/></div>
+        <div className="notification-message-reject-accept">
+          <div className="notification-message">{message}</div>
+          <div className="notification-reject-accept">
+            <div className="notification-reject" onClick={reject}>
+              <ImCross color="red" />
+            </div>
+            <div className="notification-accept" onClick={accept}>
+              <FaCheck color="greenyellow" />
+            </div>
+          </div>
         </div>
       </div>
     </>
