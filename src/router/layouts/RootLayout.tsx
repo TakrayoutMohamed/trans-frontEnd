b04fc31215b6896c709f83fa93781f4fb0bdb64c
TakrayoutMohamed <takrayoutm@gmail.com>
2024-12-31 21:02:33 +0100

    Update : make a custom hook for starting sockets to use it in the chat with the same way

--- src/router/layouts/RootLayout.tsx
+++ src/router/layouts/RootLayout.tsx
@@ -1,60 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useEffect } from "react";
-import { w3cwebsocket } from "websocket";
-import { RootState } from "@/src/states/store";
 import { rootLayout } from "../styles";
-import UseAxiosPrivate from "@/src/services/hooks/UseAxiosPrivate";
-import { AxiosInstance } from "axios";
-import {
-  isValidAccessToken,
-} from "@/src/pages/modules/fetchingData";
-import { useSelector } from "react-redux";
-import { openSocket } from "@/src/pages/modules/openSocket";
-import { closeSocket } from "@/src/pages/modules/closeSocket";
-import { watchSocket } from "@/src/pages/modules/watchSocket";
+import useHandleSockets from "@/src/services/hooks/useHandleSockets";
+import { w3cwebsocket } from "websocket";
 
-let axiosPrivateHook: AxiosInstance;
-
-let client: w3cwebsocket | null = null;
-
+let notificationSocket: w3cwebsocket | null = null;
 
 const RootLayout = () => {
-  axiosPrivateHook = UseAxiosPrivate();
-  const isAuthenticated = useSelector(
-    (state: RootState) => state.authenticator.value
-  );
-  const accessToken = useSelector(
-    (state: RootState) => state.accessToken.value
-  );
-  useEffect(() => {
-    if (isAuthenticated) {
-      const handleSockets = async () => {
-        console.log("json_data");
-        if (await isValidAccessToken(client)) {
-          console.log("json_data2");
-          if (!client || client.readyState === w3cwebsocket.CLOSED)
-            client = openSocket("ws/notification", accessToken + "");
-          if (client) {
-            console.log("befor watching socket");
-            watchSocket(axiosPrivateHook, client);
-            if (client.readyState === w3cwebsocket.CLOSING) {
-              console.log("socket closing");
-            }
-            if (client.readyState === w3cwebsocket.CLOSED) {
-              console.log("socket closed");
-            }
-          }
-        }
-      };
-      handleSockets();
-      return () => {
-        console.log("cleaning funtion in APPPPPPPPPPPPPPPPPPPPPP");
-        closeSocket(client) && (client = null);
-      };
-    }
-  }, [accessToken]);
+  useHandleSockets({urlOfSocket : "ws/notification", client : notificationSocket});
   return (
     <>
       <div className={rootLayout}>
