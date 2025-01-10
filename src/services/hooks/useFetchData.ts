a3eba40d2acf66be67f523b9174c283561299836
TakrayoutMohamed <takrayoutm@gmail.com>
2025-01-10 04:27:15 +0100

    Update : setting the data of the users fetched as it should be

--- src/services/hooks/useFetchData.ts
+++ src/services/hooks/useFetchData.ts
@@ -1,12 +1,18 @@
 import { useCallback, useEffect, useState } from "react";
 import { CanceledError } from "../api/axios";
 import { AxiosResponse } from "axios";
+import { scrollDirectionTypes } from "./useInfiniteScroll";
 
 interface useFetchDataProps<T> {
   data?: T[];
   setData?: (a: T[]) => void;
-  gettingData: (url: string, page?: number, username?: string) => Promise<AxiosResponse<any, any>>;
+  gettingData: (
+    url: string,
+    page?: number,
+    username?: string
+  ) => Promise<AxiosResponse<any, any>>;
   username?: string;
+  scrollDirection?: scrollDirectionTypes;
   url: string;
 }
 
@@ -15,6 +21,7 @@ export const useFetchData = <T>({
   setData,
   data,
   username,
+  scrollDirection = "bottom",
   url,
 }: useFetchDataProps<T>) => {
   const [page, setPage] = useState<number>(1);
@@ -25,41 +32,58 @@ export const useFetchData = <T>({
     setPage(1);
     setIsLoading(false);
     setHasMore(true);
-    fetchData(1)
+    fetchData(1);
   }, [username]);
-  useEffect(() => {
-
-  },[])
-  const fetchData = useCallback(async (page: number) => {
-    if (isLoading || (!hasMore && page !== 1)) return;
-    setIsLoading(true);
-    try {
-      const res = await gettingData(url, page, username)
-      setData &&
-        setData(
-          page === 1
-            ? res.data.results.reverse()
-            : data
-            ? [...res.data.results.reverse(), ...data]
-            : res.data.results.reverse()
+  useEffect(() => {}, []);
+  const fetchData = useCallback(
+    async (page: number) => {
+      if (isLoading || (!hasMore && page !== 1)) return;
+      setIsLoading(true);
+      try {
+        const res = await gettingData(url, page, username);
+        console.log(res.data);
+        let tmp: T[];
+        if (scrollDirection === "top") {
+          if (page === 1) {
+            tmp = res.data.results.reverse();
+          } else {
+            if (data && data.length) {
+              tmp = [...res.data.results.reverse(), ...data];
+            } else {
+              tmp = res.data.results.reverse();
+            }
+          }
+        } else {
+          if (page === 1) {
+            tmp = res.data.results.users;
+          } else {
+            if (data && data.length) {
+              tmp = [...data, ...res.data.results.users];
+            } else {
+              tmp = res.data.results.users;
+            }
+          }
+        }
+        setData && setData(tmp);
+        setHasMore(res.data.next !== null);
+        setPage(page + 1);
+        setIsLoading(false);
+      } catch (err) {
+        setIsLoading(false);
+        if (err instanceof CanceledError) return;
+        console.log(
+          "In Catch err is loading : " +
+            isLoading +
+            " hasMore: " +
+            hasMore +
+            " page : " +
+            page
         );
-      setHasMore(res.data.next !== null);
-      setPage(page + 1);
-      setIsLoading(false);
-    } catch (err) {
-      setIsLoading(false);
-      if (err instanceof CanceledError) return;
-      console.log(
-        "In Catch err is loading : " +
-          isLoading +
-          " hasMore: " +
-          hasMore +
-          " page : " +
-          page
-      );
-      console.error("Error fetching data:");
-      console.error(err);
-    }
-  }, [page, isLoading, hasMore, username])
+        console.error("Error fetching data:");
+        console.error(err);
+      }
+    },
+    [page, isLoading, hasMore, username]
+  );
   return { page, isLoading, hasMore, fetchData };
 };
